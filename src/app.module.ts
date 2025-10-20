9dc955aeeba1f0fda19899a1051cf0e4cf657264
danielaguardad0 <danielamguatdado@gmail.com>
2025-10-19 22:30:20 -0600

    Import TutorsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TutorsModule } from './tutors/tutors.module';
 
 @Module({ imports: [UsersModule, AuthModule] })
 export class AppModule {}
